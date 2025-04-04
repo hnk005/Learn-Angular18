ab15c2069f93ea9032076bb41022beeff20f9293
hnk005 <huynhnamkha512020@gmail.com>
2025-04-04 15:11:58 +0700

    Complete set up a form using a template-driven approach

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   @Input() occupation = '';
+  username = 'youngTech';
+  favoriteFramework = '';
+  
 }
