8c59bfdedc3acc0e0f3c6da53842758aadc98f1b
hnk005 <huynhnamkha512020@gmail.com>
2025-04-04 13:31:34 +0700

    Complete compose components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
+  imports: [UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
